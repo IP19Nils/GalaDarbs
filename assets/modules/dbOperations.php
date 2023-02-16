61cb89e7daf59b6437fb4a976f3b7b24a05a15bf
IP19Nils <116562053+IP19Nils@users.noreply.github.com>
2023-02-16 13:57:03 +0200

    Create dbOperations.php

---
+++ assets/modules/dbOperations.php
@@ -0,0 +1,27 @@
+<?php
+
+function insert($sql, $conn) {
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function select($sql, $conn) {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        return $result;
+    } else {
+        return false;
+    }
+}
+
+function update($sql, $conn) {
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
